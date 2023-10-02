3b078d2ecccb3140b353270d8ad413a20107f8a1
germano4 <113357462+germano4@users.noreply.github.com>
2023-10-02 12:33:11 -0300

    Add files via upload

--- call.js
+++ call.js
@@ -1,3 +1,27 @@
-const form = document.querySelector('form')
 
-//calcular peso ideal 
+const form = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+//se comunicanocom o html
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const nome = form.inNome.value
+    const masculino = form.inMasculino.checked
+    const altura = Number(form.inAltura.value)
+
+    //calculando
+    let peso
+    if (masculino) {
+        peso = 22 * Math.pow(altura, 2)
+    } else {
+        peso = 21 * Math.pow(altura, 2)
+    }
+    //resultado 
+    resp.innerText = `${nome} seu peso ideal é: ${peso.toFixed(3)}Kg`
+})
+
+//limpa o form
+form.addEventListener("reset", (e) => {
+    resp.innerText = ""
+})
